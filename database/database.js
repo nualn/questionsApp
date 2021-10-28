551c457e9cc663eccea8a45e4b8c34d2e1bcd74d
nuutti <nuuttinikkola1@gmail.com>
2021-10-28 17:07:27 +0300

    Set up database credentials

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 import { Pool } from "../deps.js";
+import { databaseCreds } from "../config/databaseConfig.js";
 
 const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool({
-}, CONCURRENT_CONNECTIONS);
+const connectionPool = new Pool(databaseCreds /* this is for initial testing, will change later*/, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, ...args) => {
   const response = {};
