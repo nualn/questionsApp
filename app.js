3a3146bc4cfb65bf4411c83e0f2451a974542d6e
nualn <nuuttinikkola1@gmail.com>
2021-10-30 03:20:52 +0300

    Added authMiddleware to prevent non-logged in users from accessing parts of the site

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import renderMiddleware from "./middlewares/renderMiddleware.js";
 import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
 import { router } from "./routes/routes.js";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 const app = new Application({
   serverConstructor: HttpServerStd,
@@ -10,6 +11,7 @@ const app = new Application({
 new OakSession(app);
 
 app.use(errorMiddleware);
+app.use(authMiddleware);
 app.use(serveStaticMiddleware);
 app.use(renderMiddleware);
 app.use(router.routes());
