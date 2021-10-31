514847d3c0125952e73d5ea8ab7565402437bafb
nualn <nuuttinikkola1@gmail.com>
2021-10-31 22:01:17 +0200

    Made small adjustments to the api so that it passes the course's automated tests

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-import { Application, HttpServerStd, OakSession } from "./deps.js";
+import { Application, Session } from "./deps.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import renderMiddleware from "./middlewares/renderMiddleware.js";
 import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
 import { router } from "./routes/routes.js";
 import { authMiddleware } from "./middlewares/authMiddleware.js";
 
-const app = new Application({
-  serverConstructor: HttpServerStd,
-});
-new OakSession(app);
+const app = new Application();
+const session = new Session();
+app.use(session.initMiddleware());
 
 app.use(errorMiddleware);
 app.use(authMiddleware);
--- deps.js
+++ deps.js
@@ -1,11 +1,12 @@
 export { configure, renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
 export {
   Application,
-  HttpServerStd,
   Router,
   send,
-} from "https://deno.land/x/oak@v8.0.0/mod.ts";
-export { Pool } from "https://deno.land/x/postgres@v0.12.0/mod.ts";
-export { OakSession } from "https://deno.land/x/sessions@v1.5.4/mod.ts";
+} from "https://deno.land/x/oak@v9.0.1/mod.ts";
+export { Pool } from "https://deno.land/x/postgres@v0.13.0/mod.ts";
+export { Session } from "https://deno.land/x/oak_sessions@v3.1.3/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 export * as validasaur from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
+
+
