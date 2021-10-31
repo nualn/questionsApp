97c4f8dbfd40925f1e38f61a799bc889710ab9ef
nualn <nuuttinikkola1@gmail.com>
2021-10-31 21:04:37 +0200

    Modified restricted paths to match the requirements

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-const restrictedPaths = ["/questions"];
+const restrictedPaths = ["/questions", "/quiz", "/statistics"];
 
 const authMiddleware = async (context, next) => {
     const user = await context.state.session.get("user");
